abf4e99f6a74db145bc09219d5c39140b763b845
Ruheni <aleruheni@outlook.com>
2019-10-10 09:03:30 +0300

    removed react script tags, moved pet to own file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-const Pet = ({ name, animal, breed }) => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, name),
-		React.createElement('h2', {}, animal),
-		React.createElement('h2', {}, breed)
-	]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet  from "./Pet";
 
 const App = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me!!'),
-		React.createElement(Pet, { name: 'Remmy Williams', animal: 'Cat', breed: '....hmmm' }),
-		React.createElement(Pet, { name: 'Tyronne', animal: 'Parrot', breed: 'IDK' }),
-		React.createElement(Pet, { name: 'Ginger', animal: 'Dog', breed: 'German Shephard' })
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, "Adopt Me!!"),
+		React.createElement(Pet, {
+			name: "Remmy Williams",
+			animal: "Cat",
+			breed: "....hmmm"
+		}),
+		React.createElement(Pet, {
+			name: "Tyronne",
+			animal: "Parrot",
+			breed: "IDK"
+		}),
+		React.createElement(Pet, {
+			name: "Ginger",
+			animal: "Dog",
+			breed: "German Shephard"
+		})
 	]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+render(React.createElement(App), document.getElementById("root"));
